2252a7cfe4e42352dbf1128ca78461a931fd01a4
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-10-13 23:33:57 +0200

    replaced tabs with whitespaces

--- app/controller/Map.js
+++ app/controller/Map.js
@@ -1,30 +1,30 @@
 Ext.define('LeafletMapDemo.controller.Map', {
     extend: 'Ext.app.Controller',
-	
+
     config: {
         refs: {
-			mapCmp: '#leafletmap'
+            mapCmp: '#leafletmap'
         },
-		control: {
-			mapCmp: {
-				maprender: 'onMapRender',
-				zoomend: 'onZoomEnd',
-				movestart: 'onMoveStart',
-				moveend: 'onMoveEnd'
-			}
-		}
+        control: {
+            mapCmp: {
+                maprender: 'onMapRender',
+                zoomend: 'onZoomEnd',
+                movestart: 'onMoveStart',
+                moveend: 'onMoveEnd'
+            }
+        }
     },
-	
-	onMapRender: function(component, map, layer) {
-		console.log("map render");
-	},
-	onZoomEnd: function(component, map, layer, zoom) {
-		console.log("zoom end -> new zoom level: " + zoom);
-	},
-	onMoveStart: function(component, map, layer) {
-		console.log("move start");
-	},
-	onMoveEnd: function(component, map, layer) {
-		console.log("move end");
-	}
+
+    onMapRender: function(component, map, layer) {
+        console.log("map render");
+    },
+    onZoomEnd: function(component, map, layer, zoom) {
+        console.log("zoom end -> new zoom level: " + zoom);
+    },
+    onMoveStart: function(component, map, layer) {
+        console.log("move start");
+    },
+    onMoveEnd: function(component, map, layer) {
+        console.log("move end");
+    }
 });
